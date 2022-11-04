05a2c0132177360c5a747c3225f9d5f9ba623c7c
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2022-11-05 04:46:52 +0800

    lol

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div>
-      <Navbar bg='dark' variant='dark'>
+      <Navbar bg='danger' variant='dark'>
         <Container>
           <Navbar.Brand href='#home'>Navbar</Navbar.Brand>
           <Nav className='me-auto'>
--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -3,7 +3,7 @@ import { Button, Form } from 'react-bootstrap'
 import './Registration.css'
 const Registration = () => {
   return (
-    <div className="d-lg-flex   registration-div">
+    <div className="d-lg-flex body-reg   registration-div">
       <section className=" w-lg-50">
      
           <img className=" w-100 h-100" src="https://i.ibb.co/DDDDhqG/IMG-9324.jpg" alt="" />
@@ -12,36 +12,36 @@ const Registration = () => {
 
  <section className=" w-75 mx-auto px-lg-5  ">
         <Form className="w-100 ">
-          <h1 className="text-danger mt-5">Please Register</h1>
-          <Form.Group className="mb-3 " >
-            <Form.Label>Phone Number</Form.Label>
-            <Form.Control className="w-75" type="phone" placeholder="Phone Number" />
-            <Form.Text className="text-muted">
-            
-            </Form.Text>
+          <h1 className="mt-5 text-light text-center mb-5">Please Register</h1>
+          <Form.Group className="mb-3 w-75 mx-auto " >
+            <Form.Label className=''>Phone Number</Form.Label>
+            <Form.Control  type="phone" placeholder="Phone Number" />
+           
           </Form.Group>
-          <Form.Group className="mb-3" >
+          <Form.Group className="mb-3 w-75 mx-auto " >
             <Form.Label>Name</Form.Label>
-            <Form.Control className="w-75" type="name" placeholder="Enter FullName" />
+            <Form.Control  type="name" placeholder="Enter FullName" />
             <Form.Text className="text-muted">
              
             </Form.Text>
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Group className="mb-3 w-75 mx-auto " controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control className="w-75" type="email" placeholder="Enter email" />
+            <Form.Control  type="email" placeholder="Enter email" />
             <Form.Text className="text-muted">
         
             </Form.Text>
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Group className="mb-3 w-75 mx-auto " controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control className="w-75" type="password" placeholder="Password" />
+            <Form.Control type="password" placeholder="Password" />
           </Form.Group>
         
-          <Button className="mb-5" variant="primary" type="submit">
+          <div className='d-flex  justify-content-center'>
+          <Button className="mb-5" variant="danger" type="submit">
             Submit
           </Button>
+          </div>
 
 
         </Form>
