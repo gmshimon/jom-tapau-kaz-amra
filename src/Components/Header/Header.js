2dc665d80749077af1691096a17b0aa6dda8cbc2
GMShimon <simon.rosedale99@gmail.com>
2022-11-05 02:20:19 +0800

    react-router added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Navbar,Container,Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+const Header = () => {
+  return (
+    <div>
+      <Navbar bg='dark' variant='dark'>
+        <Container>
+          <Navbar.Brand href='#home'>Navbar</Navbar.Brand>
+          <Nav className='me-auto'>
+            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Components/Login/LoginForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './Login';
+
+const LoginForm = () => {
+    return (
+        <div>
+            <div></div>
+            <Login></Login>
+        </div>
+    );
+};
+
+export default LoginForm;
