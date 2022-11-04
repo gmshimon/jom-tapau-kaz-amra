5c1f24f03ab2530f39a9b269bb1a116e2030dae2
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2022-11-05 03:57:04 +0800

    registration added

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -1,11 +1,54 @@
-import React from 'react';
-
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import "./Registration.css";
 const Registration = () => {
-    return (
-        <div>
+  return (
+    <div className="d-lg-flex   registration-div">
+      <section className=" w-lg-50">
+     
+          <img className="registration-img w-100 h-100" src="https://i.ibb.co/DDDDhqG/IMG-9324.jpg" alt="" />
+      </section>
+
+ <div className="bg-dark">
+ <section className="  mx-auto d-flex align-items-center">
+        <Form className="w-100 ">
+          <h1 className="text-danger mt-5">Please Register</h1>
+          <Form.Group className="mb-3 " controlId="formBasicEmail">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control type="phone" placeholder="Phone Number" />
+            <Form.Text className="text-muted">
             
-        </div>
-    );
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="name" placeholder="Enter FullName" />
+            <Form.Text className="text-muted">
+             
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </section>
+ </div>
+    </div>
+  );
 };
 
 export default Registration;
