f7b6661c53343e8b4138b3fa2528546c084bc4ae
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2022-11-05 04:20:23 +0800

    completed

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -6,47 +6,47 @@ const Registration = () => {
     <div className="d-lg-flex   registration-div">
       <section className=" w-lg-50">
      
-          <img className="registration-img w-100 h-100" src="https://i.ibb.co/DDDDhqG/IMG-9324.jpg" alt="" />
+          <img className=" w-100 h-100" src="https://i.ibb.co/DDDDhqG/IMG-9324.jpg" alt="" />
       </section>
 
- <div className="bg-dark">
- <section className="  mx-auto d-flex align-items-center">
+
+ <section className=" w-50 mx-auto px-lg-5  ">
         <Form className="w-100 ">
           <h1 className="text-danger mt-5">Please Register</h1>
           <Form.Group className="mb-3 " controlId="formBasicEmail">
             <Form.Label>Phone Number</Form.Label>
-            <Form.Control type="phone" placeholder="Phone Number" />
+            <Form.Control className="w-75" type="phone" placeholder="Phone Number" />
             <Form.Text className="text-muted">
             
             </Form.Text>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Name</Form.Label>
-            <Form.Control type="name" placeholder="Enter FullName" />
+            <Form.Control className="w-75" type="name" placeholder="Enter FullName" />
             <Form.Text className="text-muted">
              
             </Form.Text>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control className="w-75" type="email" placeholder="Enter email" />
             <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
+        
             </Form.Text>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control className="w-75" type="password" placeholder="Password" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
+        
+          <Button className="mb-5" variant="primary" type="submit">
             Submit
           </Button>
+
+
         </Form>
       </section>
- </div>
+
     </div>
   );
 };
