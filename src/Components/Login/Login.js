a75954935c91fc9aea43bdea424b777a57a557cb
GMShimon <simon.rosedale99@gmail.com>
2022-11-06 03:53:25 +0800

    wulala

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,11 +1,75 @@
-import React from 'react';
-
+import React from 'react'
+import './Login.css'
 const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+  return (
+    <div>
+      <div className='d-lg-flex body-reg   registration-div'>
+        <section className=' w-lg-50'>
+          <img
+            className=' w-100 h-100'
+            src='https://i.ibb.co/Dg3F3FV/IMG-9325.jpg'
+            alt=''
+          />
+        </section>
+        <section className=' w-75 mx-auto px-lg-5  '>
+          <div class='login-container'>
+            <div class='did-floating-label-content'>
+              <input class='did-floating-input' type='text' placeholder=' ' />
+              <label class='did-floating-label'>Sale Price</label>
+            </div>
+            <div class='did-floating-label-content did-error-input'>
+              <input class='did-floating-input' type='text' placeholder=' ' />
+              <label class='did-floating-label'>Sale Price</label>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  )
+}
+export default Login
 
-export default Login;
+{
+  /* <section className=" w-lg-50">
+     
+     <img className=" w-100 h-100" src="https://i.ibb.co/DDDDhqG/IMG-9324.jpg" alt="" />
+ </section>
+
+
+<section className=" w-75 mx-auto px-lg-5  ">
+   <Form className="w-100 ">
+     <h1 className="mt-5 text-light text-center mb-5">Please Register</h1>
+     <Form.Group className="mb-3 w-75 mx-auto " >
+       <Form.Label className=''>Phone Number</Form.Label>
+       <Form.Control  type="phone" placeholder="Phone Number" />
+      
+     </Form.Group>
+     <Form.Group className="mb-3 w-75 mx-auto " >
+       <Form.Label>Name</Form.Label>
+       <Form.Control  type="name" placeholder="Enter FullName" />
+       <Form.Text className="text-muted">
+        
+       </Form.Text>
+     </Form.Group>
+     <Form.Group className="mb-3 w-75 mx-auto " controlId="formBasicEmail">
+       <Form.Label>Email address</Form.Label>
+       <Form.Control  type="email" placeholder="Enter email" />
+       <Form.Text className="text-muted">
+   
+       </Form.Text>
+     </Form.Group>
+     <Form.Group className="mb-3 w-75 mx-auto " controlId="formBasicPassword">
+       <Form.Label>Password</Form.Label>
+       <Form.Control type="password" placeholder="Password" />
+     </Form.Group>
+   
+     <div className='d-flex  justify-content-center'>
+     <Button className="mb-5" variant="danger" type="submit">
+       Submit
+     </Button>
+     </div>
+
+
+   </Form>
+ </section> */
+}
