84c1f7b95b33b9b96699ecf2ba4e231670727201
GMShimon <simon.rosedale99@gmail.com>
2022-11-06 05:36:08 +0800

    login-registration

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 import './Login.css'
 const Login = () => {
   return (
-    <div>
+    <div style={{ backgroundColor: 'rgba(117, 131, 136, 0.2)' }}>
       <div className='d-lg-flex body-reg   registration-div'>
         <section className=' w-lg-50'>
           <img
@@ -11,16 +12,45 @@ const Login = () => {
             alt=''
           />
         </section>
-        <section className=' w-75 mx-auto px-lg-5  '>
-          <div class='login-container'>
-            <div class='did-floating-label-content'>
-              <input class='did-floating-input' type='text' placeholder=' ' />
-              <label class='did-floating-label'>Sale Price</label>
+        <section className='w-75 mx-auto px-lg-5'>
+          <h1 style={{fontSize:'80px'}} className='waviy text-center mt-5 mb-2 fst-italic'>
+            <span style={{'--i':1}} className='shadow-red text-white'>J</span>
+            <span style={{'--i':2}} className='shadow-white text-danger'>om</span>
+            <small></small>
+            <span yle={{'--i':3}} className='shadow-red text-white'>Ta</span>
+            <span yle={{'--i':4}} className='shadow-white text-danger'>pa</span>
+            <span yle={{'--i':5}} className='shadow-red text-white'>u</span>
+          </h1>
+          <div yle={{'--i':6}} className='d-flex justify-content-center'>
+            <div>
+            <p className='text-center mt-5 fs-3'>Fill out the Form to login</p>
+            <hr style={{width:'400px',color:'green',border:'2px solid green'}}/>
             </div>
-            <div class='did-floating-label-content did-error-input'>
-              <input class='did-floating-input' type='text' placeholder=' ' />
-              <label class='did-floating-label'>Sale Price</label>
+          </div>
+          <form className='w-100 '>
+            <div className='login-container'>
+              <div className='did-floating-label-content'>
+                <input className='did-floating-input' type='email' placeholder=' ' />
+                <label className='did-floating-label'>Email</label>
+              </div>
+              <div className='did-floating-label-content did-error-input'>
+                <input className='did-floating-input' type='password' placeholder=' ' />
+                <label className='did-floating-label'>Password</label>
+              </div>
             </div>
+            <div className='d-flex  justify-content-center'>
+              <Button className='mb-5' variant='danger' type='submit'>
+                Login
+              </Button>
+            </div>
+          </form>
+          <div className='d-flex justify-content-center '>
+            <hr style={{width:'200px',color:'red'}}/>
+            <p className='px-3 pt-1'>or</p>
+            <hr style={{width:'200px',color:'red'}}/>
+          </div>
+          <div className='d-flex justify-content-center'>
+            <Button style={{width:'200px'}}  variant="success">Google Sign</Button>
           </div>
         </section>
       </div>
@@ -28,48 +58,3 @@ const Login = () => {
   )
 }
 export default Login
-
-{
-  /* <section className=" w-lg-50">
-     
-     <img className=" w-100 h-100" src="https://i.ibb.co/DDDDhqG/IMG-9324.jpg" alt="" />
- </section>
-
-
-<section className=" w-75 mx-auto px-lg-5  ">
-   <Form className="w-100 ">
-     <h1 className="mt-5 text-light text-center mb-5">Please Register</h1>
-     <Form.Group className="mb-3 w-75 mx-auto " >
-       <Form.Label className=''>Phone Number</Form.Label>
-       <Form.Control  type="phone" placeholder="Phone Number" />
-      
-     </Form.Group>
-     <Form.Group className="mb-3 w-75 mx-auto " >
-       <Form.Label>Name</Form.Label>
-       <Form.Control  type="name" placeholder="Enter FullName" />
-       <Form.Text className="text-muted">
-        
-       </Form.Text>
-     </Form.Group>
-     <Form.Group className="mb-3 w-75 mx-auto " controlId="formBasicEmail">
-       <Form.Label>Email address</Form.Label>
-       <Form.Control  type="email" placeholder="Enter email" />
-       <Form.Text className="text-muted">
-   
-       </Form.Text>
-     </Form.Group>
-     <Form.Group className="mb-3 w-75 mx-auto " controlId="formBasicPassword">
-       <Form.Label>Password</Form.Label>
-       <Form.Control type="password" placeholder="Password" />
-     </Form.Group>
-   
-     <div className='d-flex  justify-content-center'>
-     <Button className="mb-5" variant="danger" type="submit">
-       Submit
-     </Button>
-     </div>
-
-
-   </Form>
- </section> */
-}
