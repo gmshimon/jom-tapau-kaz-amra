30bb526b854bfb8b73fc81c9ecfbfc154220788e
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2022-11-12 03:37:14 +0800

    added hyphen

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,11 +6,12 @@ const Header = () => {
     <div>
       <Navbar bg='danger' variant='dark'>
         <Container>
-          <Navbar.Brand href='#home'>Jom Tapau</Navbar.Brand>
+          <Navbar.Brand href='#home'>Jom Tapau -</Navbar.Brand>
           <Nav className='me-auto'>
             <Nav.Link as={Link} to="/home">Home</Nav.Link>
             <Nav.Link as={Link} to="/login">Login</Nav.Link>
             <Nav.Link as={Link} to="/about">About</Nav.Link>
+
           </Nav>
         </Container>
       </Navbar>
